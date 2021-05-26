a50bde16f4a1849eb2d98d7fd40e6aa46615ae21
Gaston Prat <gastonemanuelprat@gmail.com>
2021-05-26 10:45:18 -0300

    Subiendo ejercicio 1

---
+++ tp5ej01.py
@@ -0,0 +1,42 @@
+################
+# Gastón Emanuel Prat - @GastonPrat
+# Trabajo Práctico N°5
+# Ejercicio 01
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+class IngresoIncorrecto(Exception):
+    pass
+def ingreso_entero(mensaje):
+    ingreso = input(mensaje + " #")
+    try:
+        entero = int(ingreso)
+    except ValueError as err:
+        raise IngresoIncorrecto("No era un número!") from err
+    return entero
+
+def par_impar(numero):
+    numero = str(numero)
+    primer_cifra = numero[-1]    
+    primer_cifra = int(primer_cifra)
+    while primer_cifra > 0:
+        primer_cifra = primer_cifra-2
+    if primer_cifra == 0:
+        resultado = True
+    else:
+        resultado = False
+    return resultado
+
+def prueba():
+    print("Esto le dira si su numero es PAR o IMPAR")
+    numero = ingreso_entero("Ingresa un número entero")
+    resultado = par_impar(numero)
+    if resultado:
+        print(resultado)
+        print(f"Su numero es PAR")
+    else:
+        print(resultado)
+        print(f"Su numero es IMPAR")
+    
+
+if __name__ == "__main__":
+    prueba()
