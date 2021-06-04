2b70ac5ff1e559df3f5b1854e86e41d8bd77e93e
Gaston Prat <gastonemanuelprat@gmail.com>
2021-06-04 12:18:35 -0300

    Cambiando la funcion para poder ajustar la ventana

--- tp5ej11.py
+++ tp5ej11.py
@@ -7,28 +7,30 @@
 from tp5ej01 import IngresoIncorrecto, ingreso_entero
 from random import randint
 
-def promedio_movil_simple(valores):
-    if valores < 3:
-        raise IngresoIncorrecto("Tienen que ser 3 valores o mas")
+def promedio_movil_simple(ventana):
+    if ventana > 10:
+        raise IngresoIncorrecto("Ingrese un valor de 10 o menor")
     lista1 = []
     lista_promedio = []
-    for i in range (valores):
+    for i in range (15):
         lista1.append(randint(1, 100))
-    valor = 0
-    while valor < (valores-2):
-        uno = lista1[valor]
-        dos = lista1[valor+1]
-        tres = lista1[valor+2]
-        valor = valor + 1
-        promedio = (uno+dos+tres)/3
-        lista_promedio.append(round(promedio, 1))
+    for n in range(16-ventana):
+        contador = n - 1
+        sumador = 0
+        vuelta = 0
+        while vuelta < ventana:
+            sumador = sumador + lista1[contador+1]
+            vuelta = vuelta + 1
+            contador = contador +1
+        sumador = sumador / ventana
+        sumador = round(sumador, 1)
+        lista_promedio.append(sumador)
     return lista1, lista_promedio
 
 def prueba():
-    print("Aquí se mostrara una lista con su promedio móvil en 3 valores")
-    valores = ingreso_entero("Ingresar cuantos valores desea promediar,"
-                             " deben ser 3 o mas")
-    lista1, lista_promedio = promedio_movil_simple(valores)
+    print("Aquí se mostrara una lista con su promedio móvil")
+    ventana = ingreso_entero("¿Cuantos valores derea promediar?")
+    lista1, lista_promedio = promedio_movil_simple(ventana)
     print("Valores a promediar:")
     print(lista1)
     print("Promedios moviles:")
